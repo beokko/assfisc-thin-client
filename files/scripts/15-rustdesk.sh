a2ae1ad4cfa2ae8c225eafec72803276d9d9f497
aurelien <github@auretienne.com>
2026-06-07 13:13:49 +0200

    bump rustdesk version + verify hash

--- files/scripts/15-rustdesk.sh
+++ files/scripts/15-rustdesk.sh
@@ -6,10 +6,11 @@ tmpdir=$(mktemp -d)
 pwd=$(pwd)
 cd "$tmpdir"
 
-rustdesk_ver="1.4.6"
+rustdesk_ver="1.4.7"
+rustdesk_sha256="5b18f2d32ebd9d990546746c46c5ffcea0852bf1eda963497da15651e83d5f20"
 rustdesk_url="https://github.com/rustdesk/rustdesk/releases/download/$rustdesk_ver/rustdesk-$rustdesk_ver-0.x86_64.rpm"
-
 curl -sSL "$rustdesk_url" -o rustdesk.rpm
+echo "$rustdesk_sha256  rustdesk.rpm" | sha256sum --check
 
 dnf install -y ./rustdesk.rpm pipewire-gstreamer
 
