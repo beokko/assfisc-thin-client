fb35ce3335cd3d4c500b7e146ccf02940da3db3a
aurelien <github@auretienne.com>
2026-06-08 23:36:58 +0200

    tweaked printing build script

--- files/scripts/16-printing.sh
+++ files/scripts/16-printing.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-dnf install -y cups cups-filters cups-browsed ipp-usb avahi
+dnf install -y cups cups-filters cups-browsed cups-printerapp avahi-tools ipp-usb avahi
 
-systemctl enable cups.service avahi-daemon.service ipp-usb.service cups-browsed.service
+systemctl enable cups.service avahi-daemon.service cups-browsed.service
 printf 'CreateIPPPrinterQueues All\n' >> /etc/cups/cups-browsed.conf
