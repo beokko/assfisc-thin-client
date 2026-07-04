c4e1f44b9a99622cd861d98fab7c2d53dbc69759
Aurélien Etienne <github@auretienne.com>
2026-07-04 12:07:43 +0000

    Added explicit cups-browsed config option (#41)

--- files/scripts/16-printing.sh
+++ files/scripts/16-printing.sh
@@ -11,6 +11,7 @@ mv /etc/cups/cups-browsed.conf /etc/cups/cups-browsed.conf.original
 # The comment line is to make sure cups-browsed's install scriptlet can never ever overwrite this in the unlikely case the package gets updated after this step.
 cat > /etc/cups/cups-browsed.conf << EOF
 # added by post scriptlet
+BrowseRemoteProtocols dnssd
 CreateIPPPrinterQueues LocalOnly
 OnlyUnsupportedByCUPS No
 AutoShutdown Off
