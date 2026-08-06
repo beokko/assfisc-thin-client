4cc847a7062afd35f9d8f4566366cfac1f40af9f
Aurélien Etienne <github@auretienne.com>
2026-08-06 05:19:08 +0000

    Bump base image & rustdesk (#48)
    * Bump base image
    
    * Update rustdesk to 1.4.9

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY *.pub /keys/
 COPY env /env
 
 # Base Image
-FROM quay.io/almalinuxorg/almalinux-bootc:10@sha256:585a7eaab4e56a11e58042a835c4d328a72d6248019f01ee7fc3746d66a6d113
+FROM quay.io/almalinuxorg/almalinux-bootc:10@sha256:0ae7069c2d9d9053f9bb7964ab4799c0ec1797ad5ebcb311643bb84865346f27
 
 ARG IMAGE_NAME
 ARG IMAGE_REGISTRY
--- files/scripts/15-rustdesk.sh
+++ files/scripts/15-rustdesk.sh
@@ -6,8 +6,8 @@ tmpdir=$(mktemp -d)
 pwd=$(pwd)
 cd "$tmpdir"
 
-rustdesk_ver="1.4.8"
-rustdesk_sha256="2a62c6009cada96237739f92d57579987d260b7b81011b95876cca6dce1bf616"
+rustdesk_ver="1.4.9"
+rustdesk_sha256="eb1b053ac5b2f774f2271f7fbbfd2ea475899f7a55135c5e172bc54b9388f108"
 rustdesk_url="https://github.com/rustdesk/rustdesk/releases/download/$rustdesk_ver/rustdesk-$rustdesk_ver-0.x86_64.rpm"
 curl -sSL "$rustdesk_url" -o rustdesk.rpm
 echo "$rustdesk_sha256  rustdesk.rpm" | sha256sum --check
